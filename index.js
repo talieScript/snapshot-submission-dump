915bc8f97bab014d3cc04e83a0e4a66d19d6c17d
Taliesin <taliesin.bowes@hotmail.co.uk>
2020-11-26 15:44:38 +0000

    got auth token

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require('body-parser')
+const axios = require('axios')
 const jsonParser = bodyParser.json()
 
 require('dotenv').config()
@@ -7,15 +8,26 @@ require('dotenv').config()
 const app = express();
 const port = process.env.PORT || "8000";
 
+const endpoint = process.env.SNAPSHOT_API_ENDPOINT;
+
 app.use(express.json({limit: '50mb'}));
 
 app.get("/", (req, res) => {
   res.status(200).send("Hello");
 });
 
-app.post("/", jsonParser, (req, res) => {
-  const debugDump = req.body;
+app.post("/", jsonParser, async (req, res) => {
+  const debugDump = req.body.debugDump;
+  const {username, password} = req.body.user;
   // get auth token
+  const authRes = await axios.post(`${endpoint}/auth/login`, 
+  { username: username.trim(), password: password.trim() })
+    .catch(error => {
+    console.log(error)
+  })
+
+  const token = authRes.data.accessToken;
+
   // parse submission to how endpoint wants them 
   // send submissisons 
   // response when done
