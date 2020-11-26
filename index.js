28a67117993bfb2e29373c11965b6967d5fd37fb
Taliesin <taliesin.bowes@hotmail.co.uk>
2020-11-26 14:06:29 +0000

    installed dotenv and axios

--- index.js
+++ index.js
@@ -2,14 +2,12 @@ const express = require("express");
 const bodyParser = require('body-parser')
 const jsonParser = bodyParser.json()
 
+require('dotenv').config()
+
 const app = express();
 const port = process.env.PORT || "8000";
 
 app.use(express.json({limit: '50mb'}));
-
-/**
- * Routes
- */
 
 app.get("/", (req, res) => {
   res.status(200).send("Hello");
@@ -17,8 +15,11 @@ app.get("/", (req, res) => {
 
 app.post("/", jsonParser, (req, res) => {
   const debugDump = req.body;
-  console.log(debugDump);
-  res.send(debugDump)
+  // get auth token
+  // parse submission to how endpoint wants them 
+  // send submissisons 
+  // response when done
+  res.send(debugDump.submissions.submissions)
 })
 
 /**
